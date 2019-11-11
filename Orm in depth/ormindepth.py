33a341c989322ddf7a234dd3f9c0da328aa2835e
BenjaminFranklin <anoshin45@gmail.com>
2019-11-10 22:19:02 -0800

    add orm in depth file

---
+++ Orm in depth/ormindepth.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://USER@localhost:5432/jane'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+  __tablename__ = 'persons'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+      return f'<User {self.id}, {self.name}>'
+
+db.create_all()
+print(Person.query.all())
+
+#Implement a query to filter all users by name 'Bob'.
+res = Person.query.filter_by(name='Bob').all()
+print(res)
+
+#Implement a LIKE query to filter the users for records with a name that includes the letter "b".
+
+#Return only the first 5 records of the query above.
+
+#Re-implement the LIKE query using case-insensitive search.
+
+#Return the number of records of users with name 'Bob'.
+
