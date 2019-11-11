6112e4c44b22d57c112b63f86f7ea473affda3fd
BenjaminFranklin <anoshin45@gmail.com>
2019-11-11 14:11:08 -0800

    List of ToDo App

--- Orm in depth/ormindepth.py
+++ Orm in depth/ormindepth.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://VasiliA@localhost:5432/jane'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user@localhost:5432/jane'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
