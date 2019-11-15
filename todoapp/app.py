dad68ea92a8fce38ebf484dd75c16312236c88ee
BenjaminFranklin <anoshin45@gmail.com>
2019-11-15 00:39:02 -0800

    chnages in file locations

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,17 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify,abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://USER@localhost:5432/jane'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+db = SQLAlchemy(app)
+
+# start it up so we can start using the Flask database migrate commands to began initializing migrations
+# upgrade/downgrade the generating migrations as well
+migrate = Migrate(app, db)
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        db.session.rollback()
+        error=True
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort (400)
+    else:
+        return jsonify(body) 
 
 @app.route('/')
 def index():
-  return render_template('index.html', data=[{
-    'description': 'Todo 1'
-  }, {
-    'description': 'Todo 2'
-  }, {
-    'description': 'Todo 3'
-  }])
+  return render_template('index2.html', data=Todo.query.all())
 
-# Alternative approach to run a Flask app: using __main__
+# run a Flask app: using __main__
 if __name__ == "__main__":
-    app.run()    
+    app.run()     
