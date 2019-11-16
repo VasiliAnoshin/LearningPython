5b6393ac07c42223f0e2fa39f2ab251b2a8af671
BenjaminFranklin <anoshin45@gmail.com>
2019-11-16 01:29:47 -0800

    add TodoList tbl

--- todoapp/app.py
+++ todoapp/app.py
@@ -12,11 +12,21 @@ db = SQLAlchemy(app)
 # upgrade/downgrade the generating migrations as well
 migrate = Migrate(app, db)
 
+class TodoList(db.Model):
+  __tablename__ = 'todolist'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable =False)
+  todos = db.relationship('Todo',backref='list', lazy=True)
+  def __repr__(self):
+    return f'<TodoList {self.id} {self.name}>'
+
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
-  completed = db.Column(db.Boolean, nullable = False, default=False)
+  completed = db.Column(db.Boolean, nullable = False, default=False)  
+  list_id = db.Column(db.Integer, db.ForeignKey
+  ('todolist.id'), nullable = False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
